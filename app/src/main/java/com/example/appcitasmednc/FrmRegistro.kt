24d18736e1078b2e0090ed434f80b9e6fd918d22
Nairo Cuadrado <nairoxd@gmail.com>
2022-11-14 20:12:39 -0500

    Version 4.0 Se importa archivo en carpeta APP (Json de Google - firebase)

--- app/src/main/java/com/example/appcitasmednc/FrmRegistro.kt
+++ app/src/main/java/com/example/appcitasmednc/FrmRegistro.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.*
+
 
 
 class FrmRegistro : AppCompatActivity() {
+    //private val marksRef = FirebaseDatabase.getInstance().getReference("marks")
+    //private val db = Firebase.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registro_main)
