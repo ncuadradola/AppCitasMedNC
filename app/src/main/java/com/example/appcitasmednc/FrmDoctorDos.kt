0150fa292ce5793116856f26caa0ec74f1bd28c1
Nairo Cuadrado <nairoxd@gmail.com>
2022-11-22 22:40:34 -0500

    Version 7.0 Primer Intento de Login

---
+++ app/src/main/java/com/example/appcitasmednc/FrmDoctorDos.kt
@@ -0,0 +1,4 @@
+package com.example.appcitasmednc
+
+class FrmDoctorDos {
+}
--- app/src/main/java/com/example/appcitasmednc/MainActivity.kt
+++ app/src/main/java/com/example/appcitasmednc/MainActivity.kt
@@ -4,8 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  redireccionar: TextView
+    lateinit var txtEmail: EditText
+    private lateinit var txtPass: EditText
+    lateinit var btnIngresar: Button
+
+    // Creando Firebase Objeto
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,11 +35,37 @@ class MainActivity : AppCompatActivity() {
             startActivity(llamaRecuperaP)
         }
 
-        val botonHome = findViewById<Button>(R.id.btnIngresar)
-        botonHome.setOnClickListener{
-            val llamaHome = Intent( this,FrmHome::class.java)
-            startActivity(llamaHome)
-        }
+        //redireccionar = findViewById(R.id.)
+        btnIngresar = findViewById(R.id.btnIngresar)
+        txtEmail = findViewById(R.id.txtNombreUsuario)
+        txtPass = findViewById(R.id.txtContrasena)
 
+        auth = FirebaseAuth.getInstance()
+
+        btnIngresar.setOnClickListener {
+            login()
+        }
+//        val botonHome = findViewById<Button>(R.id.btnIngresar)
+//        botonHome.setOnClickListener{
+//            val llamaHome = Intent( this,FrmHome::class.java)
+//            startActivity(llamaHome)
+//        }
+    }
+
+    private fun login(){
+        val email = txtEmail.text.toString()
+        val pass = txtPass.text.toString()
+
+        auth.signInWithEmailAndPassword (email,pass).addOnCompleteListener(this){
+            if(it.isSuccessful){
+                val botonHome = findViewById<Button>(R.id.btnIngresar)
+                botonHome.setOnClickListener{
+                    val llamaHome = Intent( this,FrmHome::class.java)
+                    startActivity(llamaHome)
+                }
+            }
+            else
+                Toast.makeText(this,"Algo Falló",Toast.LENGTH_LONG).show()
+        }
     }
 }
